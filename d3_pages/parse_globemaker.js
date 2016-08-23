5e63bf9d9ec636ac377f71145ccc02a5233ad8a9
David Swart <dmswart1@gmail.com>
2016-08-23 11:57:04 -0400

    working parser2

--- d3_pages/parse_globemaker.js
+++ d3_pages/parse_globemaker.js
@@ -1,20 +1,34 @@
-var skeleton = function() {
-    var result;
-    result.push = function() {}; 
-    result.pop = function() {}; 
-    result.line = function(length, strength) {}; 
-    result.move = function(length) {}; 
-    result.rotate = function(angle) {}; 
+function skeleton() {
+    this.state = [];
+    this.__planepos = {x:250, y:250};
+    this.__planedir = {x:200, y:0};
+    this.__string = 'M' + String(this.__planepos.x) + ',' + String(this.__planepos.y) + ' ';
 
-    return result;
+    this.push = function() {}; 
+    this.pop = function() {}; 
+    this.line = function(length, strength) {
+        this.__planepos.x += length * this.__planedir.x;
+        this.__planepos.y += length * this.__planedir.y;
+        this.__string += 'L' + String(this.__planepos.x) + ',' + String(this.__planepos.y) + ' ';
+    }; 
+    this.move = function(length) {
+        this.__planepos.x += length * this.__planedir.x;
+        this.__planepos.y += length * this.__planedir.y;
+        this.__string += 'L' + String(this.__planepos.x) + ',' + String(this.__planepos.y) + ' ';
+    };
+    this.rotate = function(angle) {
+        var theta = Math.atan2(this.__planedir.y, this.__planedir.x);
+        theta = theta + (angle * Math.PI);
+        this.__planedir = {x: Math.cos(theta) * 200, y: Math.sin(theta) * 200};
+    }
+
+    this.toString = function() { return this.__string; }
 }
 
-var parse_globemaker = function(skeleton_string) {
-    var result = skeleton();
-
+var parse_globemaker = function(skeleton_string, skeleton_obj) {
     // tokens
-    var __tokens = skeleton_string.split('[ \t\r\n]');
-    __tokens.append('<EOF>');
+    skeleton_string += ' <EOF>';
+    var __tokens = skeleton_string.split(/[ \t\r\n][ \t\r\n]*/);
     var __token_idx = 0;
 
     var token = function() {
@@ -22,19 +36,20 @@ var parse_globemaker = function(skeleton_string) {
     };
     var next_token = function() {
         if(token() !== '<EOF>') {
-            token_idx++;
+            __token_idx++;
         }
     };
 
     // terminals
     var terminal = function(str) {
-        if( toupper(str) === toupper(token())) {next_token(); return true;}
+        if( str.toUpperCase() === token().toUpperCase()) {next_token(); return true;}
         return false;
     };
 
-    var value = function(value_out) {
-        value_out = parseFloat(token());
-        if( value_out !== NaN ) { next_token(); return true;}
+    var __value = 0;
+    var value = function() {
+        __value = parseFloat(token());
+        if( !isNaN(__value) ) { next_token(); return true;}
         return false;
     };
 
@@ -57,12 +72,15 @@ var parse_globemaker = function(skeleton_string) {
     var lineto_cmd = function() {
         if( lineto() ) {
            var length;
-           if(!value(length)) { /*TODO error*/ return false; }
+           if(!value()) { /*TODO error*/ return false; }
+           else { length = __value; }
 
            var strength;
-           if(!value(strength)) { strength = 1.0; }
+           if(!value()) { strength = 1.0; }
+           else { strength = __value; }
            
-           result.line(length, strength);
+           skeleton_obj.line(length, strength);
+           return true;
         }
         return false;
     };
@@ -70,9 +88,10 @@ var parse_globemaker = function(skeleton_string) {
     var moveto_cmd = function() {
         if( moveto() ) {
            var length;
-           if(!value(length)) { /*TODO error*/ return false; }
+           if(!value()) { /*TODO error*/ return false; }
 
-           result.move(length);
+           skeleton_obj.move(__value);
+           return true;
         }
         return false;
     };
@@ -80,9 +99,10 @@ var parse_globemaker = function(skeleton_string) {
     var rotate_cmd = function() {
         if( rotate() ) {
            var angle;
-           if(!value(angle)) { /*TODO error*/ return false; }
+           if(!value()) { /*TODO error*/ return false; }
 
-           result.rotate(angle);
+           skeleton_obj.rotate(__value);
+           return true;
         }
         return false;
     };
@@ -100,12 +120,12 @@ var parse_globemaker = function(skeleton_string) {
     
     var stacked = function() {
         if( !push() ) { return false; }
-        result.push();
+        skeleton_obj.push();
 
         if( !statements() ){ /*TODO error*/ return false; }
 
         if( !pop() ) { /*TODO error*/ return false; }
-        result.pop();
+        skeleton_obj.pop();
 
         return true;
     };
@@ -126,6 +146,6 @@ var parse_globemaker = function(skeleton_string) {
     };
 
     if (!statements()) { return false; }
-    if (terminal('<EOF>')) { /*TODO error*/ return false; }
+    if (!terminal('<EOF>')) { /*TODO error*/ return false; }
     return true;
 }
